d2fe6e587de72887ca7f9c51882a78038ad46a3b
axess <Exzeest@gmail.com>
2017-08-28 17:09:35 +0300

    Realized methods Marry and Divorce.

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -9,13 +9,30 @@ public class Person {
         this.man = man;
         this.name = name;
     }
+
+    public Person ( boolean man, String name, Person spouse) {
+        this.man = man;
+        this.name = name;
+        this.spouse = spouse;
+
+    }
     /**
      * This method checks gender of persons. If genders are not equal - tries to marry.
-     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife.
+     * Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
      * @param person - new husband/wife for this person.
-     * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
+     * @return - returns true if this person has another gender than passed
+     * person and they are not husband and wife, false otherwise
      */
     public boolean Marry(Person person) {
+        if (man != person.man){
+            if (spouse == person.spouse) return false;
+            if (spouse != null) Divorce ();
+            if (person.spouse != null) Divorce ();
+            spouse = person.spouse;
+            person.spouse = this;
+            return true;
+        }
         return false;
     }
 
@@ -24,7 +41,14 @@ public class Person {
      * @return true - if person status has been changed
      */
     public boolean Divorce() {
+        if (spouse != null) {
+            spouse.spouse = null;
+            spouse = null;
+            return true;
+        }
         return false;
+
+
     }
 
 }
